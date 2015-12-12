60535e5ea028d805ad6d955ed430192d731e6df5
川 青 <ncqingchuan@126.com>
2015-12-12 16:36:48 +0800

    2015-12-12

---
+++ Hyde.Api/Config/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+namespace Hyde.Api.Host.Config
+{
+    public class RouteConfig
+    {
+        public static void RouteRegister(HttpConfiguration config)
+        {
+            var routeTables = config.Routes;
+
+            routeTables.MapHttpRoute("DefaultApi",
+                routeTemplate: "api/{controller}/{action}/Key",
+                defaults: new { Key = RouteParameter.Optional });
+        }
+    }
+}
