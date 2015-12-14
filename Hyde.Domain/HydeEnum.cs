09d193d4ff46b1c3c7b095be091c116f2264e670
川 青 <ncqingchuan@126.com>
2015-12-14 14:54:27 +0800

    hyde

---
+++ Hyde.Domain/HydeEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyde.Domain
+{
+    public enum ORDER_STATE
+    {
+        Create = 1,
+        Sorting = 2,
+        Cancled = 3
+
+
+    }
+
+    public enum STOCK_DIRECTOR
+    {
+        InStock = 1,
+        OutStock = -1
+    }
+}
